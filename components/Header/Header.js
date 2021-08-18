3de889a915e044bbc4a5ea2be861bcbbe00162b2
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2021-08-19 00:41:56 +0100

    Printed Name and Added HNG, Zuri Photos

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -7,6 +7,7 @@ import { DiCssdeck } from 'react-icons/di';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () => (
+
 	<Container>
 		<Div1>
 			<Link href='/'>
--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+	Section,
+	SectionText,
+	SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <>
-    <Section row nopadding>
-      <LeftSection>
-        <SectionTitle main center>
-          Hello This Is <br />
-          Obi Nkemjika Derick
-        </SectionTitle>
-        <SectionText>
-          I am a Software Engineer. I love to solve problems and create new web experiences for the people.
-        </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
-      </LeftSection>
-    </Section>
-  </>
+const showName = () => {
+	alert('Obi Nkemjika Derick');
+};
+
+const Hero = props => (
+	<>
+		<Section row nopadding>
+			<LeftSection>
+				<SectionTitle main center>
+					Hello This Is <br />
+					Obi Nkemjika Derick
+				</SectionTitle>
+				<SectionText>
+					I am a Software Engineer. I love to solve problems and create new web
+					experiences for the people.
+				</SectionText>
+				<Button onClick={showName}>Print Name</Button>
+			</LeftSection>
+		</Section>
+	</>
 );
 
 export default Hero;
